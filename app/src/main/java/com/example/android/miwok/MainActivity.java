ae5f02260f8b6576dd45c3ef5b6ae5b6386d0441
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-04 17:05:18 -0600

    Forma final de llamar otras views desde el codigo de java sin hacer una clase aparte y sin hacer referencia al codigo XML

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -31,23 +31,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-
         /**
          * Crea un nuevo objeto del tipo llamado clicklistener del tipo definido en el
          * constructor que esta en NumbersClickListener usando el constructor
          * NumbersClickListener clickListener = new NumbersClickListener; esta version crea
-         * el objeto inline cuando se llama al metodo de la interfaz
-         */
-
-        /**
+         * el objeto inline cuando se llama al metodo de la interfaz (old)
          * Encuentra la View que muestra la viste llamada numbers con el metodo find view byid
          */
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView numbersView = (TextView) findViewById(R.id.numbers);
         /**
-         * Establece un mètodo set on click listener y el objeto creado
-         * con el constructor de numbersclicklistener como el input
+         * Establece un metodo de una interfaz nueva que se va a llamar del objeto OnClickListener
+         * y despues se llama al overrride para implementar el compartamiento del metodo OnClick
+         * que empieza la actividad number (esto es para no hacer una nueva clase en un archivo
+         * separado
          */
-        numbers.setOnClickListener(new NumbersClickListener());
+        numbersView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numbersActivity = new Intent(MainActivity.this, Numbers.class);
+                startActivity(numbersActivity);
+            }
+        });
     }
 
     /**codigo para llamar a otra view haciendo referencia a un xml
