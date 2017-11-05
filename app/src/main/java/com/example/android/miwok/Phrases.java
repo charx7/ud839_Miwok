3af2b9d460d9ba42e8da2690fe5e3d9ff5a50380
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-04 22:07:00 -0600

    Añadida la misma funcionalidad pasada pero ahora de la vista Frases

--- app/src/main/java/com/example/android/miwok/Phrases.java
+++ app/src/main/java/com/example/android/miwok/Phrases.java
@@ -2,6 +2,16 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class Phrases extends AppCompatActivity {
 
@@ -9,5 +19,27 @@ public class Phrases extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+        ArrayList<Word> palabras = new ArrayList<Word>();
+        /**
+         * Añade la lista de palabras en ingles y miwok
+         */
+        palabras.add(new Word("Where are you going?", "minto wuksus"));
+        palabras.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        palabras.add(new Word("My name is...", "oyaaset..."));
+        palabras.add(new Word("How are you feeling?", "michәksәs?"));
+        palabras.add(new Word("I’m feeling good.", "kuchi achit"));
+        palabras.add(new Word("Are you coming?", "әәnәs'aa?"));
+        palabras.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
+        palabras.add(new Word("Let’s go.", "yoowutis"));
+        palabras.add(new Word("Come here.", "ama"));
+        palabras.add(new Word("Yes, I’m coming.\n", "hәә’ әәnәm"));
+
+        /**
+         * Manera chevere de crear los views de una lista rapido y con memoria reciclandose
+         *  ahora con un adaptador custom
+         */
+        WordAdapter Adapter = new WordAdapter(this, palabras);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(Adapter);
     }
 }
