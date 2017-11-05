95e345d76c970d60409c7b0b2256d5a646e142a6
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-04 22:01:36 -0600

    Añadida la misma funcionalidad pasada pero ahora de la vista Familia

--- app/src/main/java/com/example/android/miwok/Family_Members.java
+++ app/src/main/java/com/example/android/miwok/Family_Members.java
@@ -2,6 +2,16 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class Family_Members extends AppCompatActivity {
 
@@ -9,5 +19,27 @@ public class Family_Members extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family__members);
+        ArrayList<Word> palabras = new ArrayList<Word>();
+        /**
+         * Añade la lista de palabras en ingles y miwok
+         */
+        palabras.add(new Word("father", "әpә"));
+        palabras.add(new Word("mother", "әṭa"));
+        palabras.add(new Word("son", "angsi"));
+        palabras.add(new Word("daughter", "tune"));
+        palabras.add(new Word("older brother", "taachi"));
+        palabras.add(new Word("younger brother", "chalitti"));
+        palabras.add(new Word("older sister", "teṭe"));
+        palabras.add(new Word("younger sister", "kolliti"));
+        palabras.add(new Word("grandmother", "ama"));
+        palabras.add(new Word("grandfather", "paapa"));
+
+        /**
+         * Manera chevere de crear los views de una lista rapido y con memoria reciclandose
+         *  ahora con un adaptador custom
+         */
+        WordAdapter Adapter = new WordAdapter(this, palabras);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(Adapter);
     }
 }
